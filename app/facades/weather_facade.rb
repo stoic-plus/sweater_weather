c05c6d614d9b24d185649baefbbcedefe9b04bc5
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-22 13:34:47 -0700

    adds WeatherFacade - using service to create Forecast using json and Weather objects

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -1,5 +1,21 @@
 class WeatherFacade
   def self.get_forecast(city_state)
-    Forecast
+    weather_json = WeatherService.get_forecast(get_coordinates(city_state))
+    Forecast.new(weather_attributes(weather_json))
+  end
+
+  private
+
+  def self.get_coordinates(city_state)
+    GeocodingService.get_lat_lng(city_state)
+  end
+
+  def self.weather_attributes(weather_json)
+    {
+      currently: Weather.new(weather_json[:currently]),
+      today_summary: weather_json[:daily][:summary],
+      hourly: weather_json[:hourly][:data].map{|weather_info| Weather.new(weather_info) },
+      daily: weather_json[:daily][:data].map{|weather_info| Weather.new(weather_info) }
+    }
   end
 end
