ff8d6f94b4d3e3ceaa8968611c60a4891065e721
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-23 15:07:11 -0700

    adds POROs for api errors and keys

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,19 @@ module ApplicationHelper
   class ApiError
     @@count = 0
     attr_reader :message, :id
-    def initialize(message)
+    def initialize(attributes)
       @id = @@count
       @@count += 1
-      @message = message
+      @message = attributes[:message]
+    end
+  end
+  class ApiKey
+    @@count = 0
+    attr_reader :api_key, :id
+    def initialize(api_key)
+      @id = @@count
+      @@count += 1
+      @api_key = api_key
     end
   end
 end
