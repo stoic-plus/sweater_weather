30e962738ff799cfdb4ccdf0327c1b9cfe11bfc9
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-23 14:28:48 -0700

    adds spec for user login req - sending valid and invalid data

---
+++ spec/requests/user_login_request_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+describe 'User Login', type: :request do
+  context 'when sending a POST to /api/v1/sessions with valid email and password' do
+    before :each do
+      @user = User.create(email: "whatever@example.com", password: "password")
+    end
+    it 'returns appropriate JSON' do
+      body = {
+        "email" => "whatever@example.com",
+        "password" => "password",
+      }
+      headers = {
+        "Content-Type" => "application/json",
+        "Accept" => "application/json"
+      }
+      post '/api/v1/sessions', params: { user: body , headers: headers }
+
+      json = JSON.parse(response.body)
+      expect(json["status"]).to eq(200)
+      expect(json["api_key"]).to eq(user.api_key)
+      expect(response).to be_successful
+      expect(response.content_type).to eq("application/json")
+      expect(response.status).to eq(200)
+    end
+  end
+  context 'when sending a POST to /api/v1/sessions with valid email and password' do
+    it 'returns appropriate JSON' do
+      body = {
+        "email" => "whatever@example.com",
+        "password" => "password123",
+      }
+      headers = {
+        "Content-Type" => "application/json",
+        "Accept" => "application/json"
+      }
+      post '/api/v1/sessions', params: { user: body , headers: headers }
+
+      json = JSON.parse(response.body)
+      expect(json["status"]).to eq(400)
+      expect(json).to_not have_key("api_key")
+      expect(response.content_type).to eq("application/json")
+      expect(response.status).to eq(400)
+    end
+  end
+end
