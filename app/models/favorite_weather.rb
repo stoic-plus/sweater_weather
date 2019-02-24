7cabdb53a2a1351e2ab9a4c8ad2aaaa4a19ee9e4
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-24 12:44:59 -0700

    adds id to FavoriteWeather - slight change in passed location string

--- app/models/favorite_weather.rb
+++ app/models/favorite_weather.rb
@@ -1,7 +1,9 @@
 class FavoriteWeather
-  attr_reader :location, :current_weather
-
+  attr_reader :id, :location, :current_weather
+  @@count = 0
   def initialize(location, current_weather)
+    @id = @@count
+    @@count += 1
     @location = location
     @current_weather = current_weather
   end
