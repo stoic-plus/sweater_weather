f7d5a8d3ad76bdae25b72992a3bfb94f295d944f
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-24 11:07:05 -0700

    adds spec for CurrentWeather.for_location

--- spec/models/current_weather_spec.rb
+++ spec/models/current_weather_spec.rb
@@ -1,6 +1,30 @@
 require 'rails_helper'
 
 describe CurrentWeather, type: :model do
+  describe 'class methods' do
+    context '.for_location' do
+      it 'returns instance of itself given lat and lng', :vcr do
+        current_weather_json = WeatherService.get_current_weather(lat: "39.7392358", lng: "-104.990251")
+        current_weather = CurrentWeather.for_location(lat: "39.7392358", lng: "-104.990251")
+
+        expect(current_weather).to be_a(CurrentWeather)
+
+        expect(current_weather.time).to eq(current_weather_json[:time])
+        expect(current_weather.summary).to eq(current_weather_json[:summary])
+        expect(current_weather.icon).to eq(current_weather_json[:icon])
+        expect(current_weather.precipProbability).to eq(current_weather_json[:precipProbability])
+        expect(current_weather.temperature).to eq(current_weather_json[:temperature])
+        expect(current_weather.apparentTemperature).to eq(current_weather_json[:apparentTemperature])
+        expect(current_weather.humidity).to eq(current_weather_json[:humidity])
+        expect(current_weather.pressure).to eq(current_weather_json[:pressure])
+        expect(current_weather.uvIndex).to eq(current_weather_json[:uvIndex])
+        expect(current_weather.visibility).to eq(current_weather_json[:visibility])
+        expect(current_weather.ozone).to eq(current_weather_json[:ozone])
+        expect(current_weather.nearestStormDistance).to eq(current_weather_json[:nearestStormDistance])
+        expect(current_weather.nearestStormBearing).to eq(current_weather_json[:nearestStormBearing])
+      end
+    end
+  end
   it 'can be initialized with attributes', :vcr do
     weather = WeatherService.get_current_weather(lat: "39.7392358", lng: "-104.990251")
 
