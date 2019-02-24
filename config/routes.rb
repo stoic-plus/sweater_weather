7f260ee8cd566ff4db8fa2ce3e9d22fe77e02423
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-24 16:40:25 -0700

    adds backgrounds route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/forecast', to: "forecast#show"
+      get '/backgrounds', to: "background#show"
       resources :users, only: [:create]
       resources :sessions, only: [:create]
       resources :favorites, only: [:index, :create]
