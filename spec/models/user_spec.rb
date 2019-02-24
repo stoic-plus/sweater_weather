b186796e7f3d12ec24bb0c34d0a6f8349f227e8e
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-23 17:39:32 -0700

    adds testing relationships for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,8 @@ describe User, type: :model do
     it { should validate_presence_of(:password) }
     it { should validate_presence_of(:password_confirmation).on(:create) }
   end
+  describe 'relationships' do
+    it { should have_many :favorites }
+    it { should have_many(:locations).through(:favorites)}
+  end
 end
