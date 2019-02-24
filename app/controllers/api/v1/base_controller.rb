dc09eca4b47230811ba4d7f3c6e646dfb523cc52
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-23 17:30:49 -0700

    adds create location helper method and adding that location to user

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::BaseController < ApplicationController
   include ApplicationHelper
   def render_error(attributes)
-    binding.pry
     render json: ApiMessageSerializer.new(ApiError.new(message: attributes[:message])), status: attributes[:status]
   end
 end
--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -16,9 +16,19 @@ class Api::V1::FavoritesController < Api::V1::BaseController
     end
   end
 
+  def create_location(location)
+    coordinates = GeocodingService.get_lat_lng("#{location[:city]},#{location[:state]}")
+    Location.create(
+      city: location[:city],
+      state: location[:state],
+      latitide: coordinates[:lat],
+      longitude: coordinates[:lng]
+    )
+  end
+
   def add_favorite(location, api_key)
-    
-    Location.create(city: location[:city], state: location[:state], )
+    location = create_location(location)
+    User.find_by(api_key: api_key).locations << location
   end
 
   def standardize_location(location)
