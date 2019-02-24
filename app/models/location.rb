6f6faec9396df6ee61abb02c6b9652a955917bb0
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-24 13:05:17 -0700

    moves methods from FavoritesController to Location

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,4 +3,22 @@ class Location < ApplicationRecord
   has_many :users, through: :favorites
 
   validates_presence_of :city, :state, :latitude, :longitude
+
+  def self.find_or_make_by(location)
+    city_state = standardize_location(location)
+    coordinates = GeocodingService.get_lat_lng("#{city_state[:city]},#{city_state[:state]}")
+    find_or_create_by(
+      city: city_state[:city],
+      state: city_state[:state],
+      latitude: coordinates[:lat],
+      longitude: coordinates[:lng]
+    )
+  end
+
+  private
+
+  def self.standardize_location(location)
+    city, state = location.gsub(" ", "").downcase.split(",")
+    {city: city, state: state}
+  end
 end
