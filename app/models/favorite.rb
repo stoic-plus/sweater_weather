fc1bd7ebccdb5b9305ee1b979a4d240bcc5d4a6b
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-24 14:13:18 -0700

    adds Favorite.destroy_favorite - changes destroy route

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,11 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :location
+
+  def self.destroy_favorite(api_key, location)
+    user_id = User.find_by(api_key: api_key).id
+    city, state = location.split(",")
+    location_id = Location.find_by(city: city.downcase, state: state.downcase)
+    find_by(user_id: user_id, location_id: location_id).destroy
+  end
 end
