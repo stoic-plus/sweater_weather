d25b36f0fb90c5fc9e7726a3400d70b5ada8dc4d
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-23 17:57:21 -0700

    adds specs for not creating location if exists and returning error for invalid api key

--- spec/requests/create_favorite_spec.rb
+++ spec/requests/create_favorite_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'Create Favorite', type: :request do
   context 'when sending a POST to /api/v1/favorites with location and api key' do
-    it 'responds with appropriate json and 200 status', :vcr do
+    it 'responds with appropriate json and 200 status, updates user\'s favorites, and creates location', :vcr do
       user = User.create(
         email: "whatever@example.com",
         password: "password",
@@ -14,6 +14,10 @@ describe 'Create Favorite', type: :request do
         "Accept" => "application/json"
       }
 
+      expect(user.favorites).to eq([])
+      expect(Location.count).to eq(0)
+      expect(Favorite.count).to eq(0)
+
       post '/api/v1/favorites', params: { location: "Denver, CO", api_key: user.api_key, headers: headers }
 
       json = JSON.parse(response.body)["data"]["attributes"]
@@ -21,6 +25,39 @@ describe 'Create Favorite', type: :request do
       expect(response).to be_successful
       expect(response.content_type).to eq("application/json")
       expect(response.status).to eq(200)
+
+      expect(Location.count).to eq(1)
+      expect(Favorite.count).to eq(1)
+      expect(user.locations.first).to eq(Location.first)
+    end
+    it 'responds appropriately, adds to favorites but does not create location if exists' do
+      user = User.create(
+        email: "whatever@example.com",
+        password: "password",
+        password_confirmation: "password",
+        api_key: 'ashdgashdgajhsdga2342'
+      )
+      Location.create(
+        city: 'denver',
+        state: 'co',
+        latitude: '39.7392358',
+        longitude: '-104.990251'
+      )
+
+      headers = {
+        "Content-Type" => "application/json",
+        "Accept" => "application/json"
+      }
+
+      expect(user.favorites).to eq([])
+      expect(Location.count).to eq(1)
+      expect(Favorite.count).to eq(0)
+
+      post '/api/v1/favorites', params: { location: "Denver, CO", api_key: user.api_key, headers: headers }
+
+      expect(Location.count).to eq(1)
+      expect(Favorite.count).to eq(1)
+      expect(user.locations.first).to eq(Location.first)
     end
   end
   context 'when sending a POST to /api/v1/favorites with location only' do
@@ -35,6 +72,27 @@ describe 'Create Favorite', type: :request do
       expect(json["message"]).to eq("must provide api_key")
       expect(response.content_type).to eq("application/json")
       expect(response.status).to eq(401)
+    end
+  end
+  context 'when sending a POST to /api/v1/favorites with location and invalid api key' do
+    it 'responds with appropriate json and 404 status', :vcr do
+      User.create(
+        email: "whatever@example.com",
+        password: "password",
+        password_confirmation: "password",
+        api_key: 'ashdgashdgajhsdga2342'
+      )
+
+      headers = {
+        "Content-Type" => "application/json",
+        "Accept" => "application/json"
+      }
+      post '/api/v1/favorites', params: { location: "Denver, CO", api_key: 'WRONG', headers: headers }
+
+      json = JSON.parse(response.body)["data"]["attributes"]
+      expect(json["message"]).to eq('invalid api_key')
+      expect(response.content_type).to eq("application/json")
+      expect(response.status).to eq(404)
     end
   end
 end
