cf2ca28abc9c54688e15cebff959511b8348d28c
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-25 13:59:01 -0700

    adds BackgroundSerializer and fixes typos

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::BackgroundsController < Api::V1::BaseController
   def show
-    background = Background.new(params["location"], FlickrFacade.get_background_url(location))
+    background = Background.new(params["location"], FlickrFacade.get_background_url(params["location"]))
     render json: BackgroundSerializer.new(background)
   end
 end
--- spec/requests/background_request_spec.rb
+++ spec/requests/background_request_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 describe 'Background Image Request', type: :request do
   context 'when making a GET req to /api/v1/backgrounds with valid location param' do
-    it 'returns a photo url for the location' do
+    it 'returns a photo url for the location', :vcr do
       get '/api/v1/backgrounds', params: {location: "denver,co"}
 
       expect(response).to be_successful
       expect(response.content_type).to eq("application/json")
       expect(response.status).to eq(200)
-      json = JSON.parse(response.body)["attributes"]
+      json = JSON.parse(response.body)["data"]["attributes"]
       expect(json).to have_key("location")
       expect(json).to have_key("photo_url")
     end
