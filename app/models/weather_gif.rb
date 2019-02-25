7c8f75967866b1f846c44a064a0677a07361d61d
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-25 11:44:03 -0700

    updaets WeatherGif to have id, fixes typo

--- app/models/weather_gif.rb
+++ app/models/weather_gif.rb
@@ -1,5 +1,7 @@
 class WeatherGif
+  attr_reader :id, :time, :summary, :url
   def initialize(attributes)
+    @id = attributes[:id]
     @time = attributes[:time]
     @summary = attributes[:summary]
     @url = attributes[:url]
