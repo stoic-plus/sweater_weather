9429929a5bad3d167d6ae556b49c094a8209ad51
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-25 10:01:39 -0700

    adds spec for WeatherFacade.get_daily_weather - returns array of DailyWeather

--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -25,10 +25,24 @@ describe WeatherFacade, type: :facade do
       end
     end
     context '.get_daily_weather' do
-      it 'returns DailyWeather objects for given location' do
+      it 'returns DailyWeather objects for given location', :vcr do
+        daily_weather_json = WeatherService.get_daily_weather(lat: "39.7392358", lng: "-104.990251")
+        first_day_json = daily_weather_json[:daily][:data].first
         daily_weathers = facade.get_daily_weather("denver, co")
 
         expect(daily_weathers).to all(be_a(DailyWeather))
+        first_day = daily_weathers.first
+
+        expect(first_day.time).to eq(first_day_json[:time])
+        expect(first_day.summary).to eq(first_day_json[:summary])
+        expect(first_day.icon).to eq(first_day_json[:icon])
+        expect(first_day.precipType).to eq(first_day_json[:precipType])
+        expect(first_day.temperatureHigh).to eq(first_day_json[:temperatureHigh])
+        expect(first_day.temperatureLow).to eq(first_day_json[:temperatureLow])
+        expect(first_day.apparentTemperatureHigh).to eq(first_day_json[:apparentTemperatureHigh])
+        expect(first_day.apparentTemperatureLow).to eq(first_day_json[:apparentTemperatureLow])
+        expect(first_day.temperatureMin).to eq(first_day_json[:temperatureMin])
+        expect(first_day.temperatureMax).to eq(first_day_json[:temperatureMax])
       end
     end
   end
