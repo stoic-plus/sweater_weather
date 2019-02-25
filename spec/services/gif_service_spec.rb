6de7f0f25a68ce1431b87cc480c9de3a6fde6dda
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-25 10:41:42 -0700

    finishes search_for_gifs spec and adds spec for multi_search

--- spec/services/gif_service_spec.rb
+++ spec/services/gif_service_spec.rb
@@ -10,5 +10,14 @@ describe GifService, type: :service do
         expect(gifs.first.url).to match(match(/https:\/\/giphy.com\/gifs\/\w+/))
       end
     end
+    context '.multi_search' do
+      it 'calls .search_for_gifs x amount of times for some string' do
+        search = {"snow" => 3, "sunny" => 4}
+        gifs = GifService.multi_search(search)
+
+        expect(gifs).to all(be_a(Gif))
+        expect(gifs.first.url).to match(match(/https:\/\/giphy.com\/gifs\/\w+/))
+      end
+    end
   end
 end
