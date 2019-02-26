55d3dbf2218dd9dd41c0d5563cfc021b39ec8e50
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-26 08:51:41 -0700

    adds slight refactor to backgrounds controller

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::BackgroundsController < Api::V1::BaseController
   def show
-    background = Background.new(params["location"], FlickrFacade.get_background_url(params["location"]))
+    background_url = FlickrFacade.get_background_url(params["location"])
+    background = Background.new(params["location"], background_url)
     render json: BackgroundSerializer.new(background)
   end
 end
