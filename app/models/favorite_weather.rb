847ee6e7cd7292a378d3802f4e3573389bbccb45
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-26 16:40:13 -0700

    updates FavoriteWeather.from_location to work with either string or location row

--- app/models/favorite_weather.rb
+++ app/models/favorite_weather.rb
@@ -9,6 +9,7 @@ class FavoriteWeather
   end
 
   def self.from_location(location)
+    location = Location.find_or_make_by(location) unless location.class == Location
     city_state = Location.as_city_state(location)
     new(city_state, WeatherFacade.get_current_weather(city_state))
   end
