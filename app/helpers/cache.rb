7df79d0153cecf47da535428b676b2a27f0f61b6
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-26 11:06:00 -0700

    adds Cache method for reading and writing current weather

--- app/helpers/cache.rb
+++ app/helpers/cache.rb
@@ -1,6 +1,7 @@
 module Cache
   BACKGROUND_EXPIRATION = 1.day
   FORECAST_EXPIRATION = 1.hour
+  CURRENT_WEATHER_EXPIRATION = 40.minutes
   def self.read_background(location)
     Rails.cache.read("#{location}-background")
   end
@@ -12,5 +13,11 @@ module Cache
   end
   def self.write_forecast(location, forecast)
     Rails.cache.write(location, forecast, expires_in: FORECAST_EXPIRATION)
+  end
+  def self.read_current_weather(location, current_weather)
+    Rails.cache.read("#{location}-current")
+  end
+  def self.write_current_weather(location, current_weather)
+    Rails.cache.write("#{location}-current", current_weather, expires_in: CURRENT_WEATHER_EXPIRATION)
   end
 end
