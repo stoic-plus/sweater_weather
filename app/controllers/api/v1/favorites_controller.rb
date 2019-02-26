e121ad5555e9e8d7dcad37b13ee10cf48003bfdb
stoic-plus <ricardoledesmadev@gmail.com>
2019-02-26 10:23:19 -0700

    skips verifying authenticity token on controllers with POST routes

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::FavoritesController < Api::V1::BaseController
+  skip_before_action :verify_authenticity_token
   before_action :require_valid_api_key
 
   def index
--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::SessionsController < Api::V1::BaseController
+  skip_before_action :verify_authenticity_token
   def create
     user = User.find_by(email: params["email"])
 
