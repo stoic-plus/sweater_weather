b91c30e1ddc99d85c8fc23e3b66541567a92c7c7
stoic-plus <ricardoledesmadev@gmail.com>
2019-03-02 09:52:37 -0700

    changes attributes for Gif - different gif urls

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -1,8 +1,9 @@
 class Gif
-  attr_reader :id, :url, :search_string
+  attr_reader :search_string, :embed_url, :looping, :original_url
   def initialize(attributes, search_string)
     @search_string = search_string
-    @id = attributes[:id]
-    @url = attributes[:url]
+    @embed_url = attributes[:embed_url]
+    @looping = attributes[:images][:looping][:mp4]
+    @original_url = attributes[:images][:original][:url]
   end
 end
